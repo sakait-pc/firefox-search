282c126a41019e04e8730292e0188209d0268e74
sakait-pc <mhdksamd@gmail.com>
2022-10-12 11:51:03 +0900

    init: Vite React TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello, Vite + React + TypeScript</h1>
+      <button onClick={() => setCount(count => count + 1)}>
+        count is {count}
+      </button>
+    </div>
+  );
+};
+
+export default App;
